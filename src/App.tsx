7aaa126275130a71af135e618996d628961e6ef8
talha426 <talhabnrehman@gmail.com>
2025-06-19 02:59:42 +0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🎮 Shadow Console</h1>
+      <p>Welcome, Player. Your quest begins here...</p>
+    </div>
+  );
+}
+
+export default App;
